371730bedd6b898c45bfa2346fa692d5b249b5f9
Vlad <vadyapb8@gmail.com>
2024-05-18 22:06:13 +0300

    Fix red wave lines

--- Lesson2_Variables/3.py
+++ Lesson2_Variables/3.py
@@ -1,7 +1,7 @@
-#3. Самая простая задача на свете
+# 3. Самая простая задача на свете
 while True:
     print('Введите натуральное число:')
     natural_number = input()
     if natural_number.isdigit() and int(natural_number) > 1:
-        print(('Купи конструктор!\n') * int(natural_number))
+        print(('Купи конструктор!' + '\n') * int(natural_number))
         break
