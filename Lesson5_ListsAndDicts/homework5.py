430ba2ffd425e60b83b6cb0609937c696bff8180
Vlad <vadyapb8@gmail.com>
2024-05-20 20:00:48 +0300

    Add homework 5

---
+++ Lesson5_ListsAndDicts/homework5.py
@@ -0,0 +1,15 @@
+# list
+my_list = ['apple', 'pear', 'banana', 'orange', 'peach']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'kiwi'
+print(my_list)
+
+# dict
+print('\n\n')
+my_dict = {'apple': 'яблоко', 'pear': 'груша', 'banana': 'банан'}
+print(my_dict)
+print(my_dict.get('pear'))
+my_dict['peach'] = 'персик'
+print(my_dict)
