8b4e67aa9f10a2c82100e10d80c14bf5c2fa941c
Vlad <vadyapb8@gmail.com>
2024-06-03 20:27:18 +0300

    Add module_4_2 homework

---
+++ Module4/module_4_2.py
@@ -0,0 +1,30 @@
+"""
+Домашнее задание по уроку "Пространство имен"
+
+    Создайте новый проект в PyCharm
+    Запустите созданный проект
+
+Ваша задача:
+
+    Создайте новую функцию def test_function
+    Создайте другую функцию внутри функции inner_function, функция должна печатать значение "Я в области видимости
+    функции test_function"
+    Вызовите функцию inner_function внутри функции test_function
+    Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+    Полученный код напишите в ответ к домашнему заданию
+"""
+
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости  функции test_function")
+
+    inner_function()
+
+
+test_function()
+# inner_function()
+# Traceback (most recent call last):
+#   File "/home/PycharmProjects/UrbanHomeworks/Module4/module_4_2.py", line 26, in <module>
+#     inner_function()
+# NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
