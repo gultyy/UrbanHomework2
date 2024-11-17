f03f62473f551bb1fbf5b92d3af79c68f2683832
Vlad <vadyapb8@gmail.com>
2024-11-17 14:10:30 +0300

    Change module_16_4 homework

--- Module16/module_16_4.py
+++ Module16/module_16_4.py
@@ -28,12 +28,14 @@ def get_all_users() -> List[User]:
 
 
 @app.post("/user/{username}/{age}")
-def create_user(new_user: User, username: Annotated[str, Path(min_length=3, max_length=20, description="Username",
+def create_user( username: Annotated[str, Path(min_length=3, max_length=20, description="Username",
                                                               example="Vladislav")],
                       age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='28')]) -> User:
-    new_user.id = len(users) + 1
-    new_user.username = username
-    new_user.age = age
+    _id = 1
+    if users:
+        _id = users[len(users) - 1].id + 1
+    new_user = User(id=_id, username=username, age=age)
+
     users.append(new_user)
     return new_user
 
@@ -43,20 +45,20 @@ def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='User ID'
                       username: Annotated[str, Path(min_length=3, max_length=20, description="Username",
                                                     example="Vladislav")],
                       age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='28')]):
-    try:
-        user: User = users[user_id - 1]
-        user.username = username
-        user.age = age
-        return user
-    except IndexError:
-        raise HTTPException(status_code=404, detail='User was not found')
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+
+    raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete("/user/{user_id}")
 def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='User ID', example='25')]) -> User:
-    try:
-        user = users[user_id - 1]
-        users.pop(user_id - 1)
-        return user
-    except IndexError:
-        raise HTTPException(status_code=404, detail='User was not found')
+    for user, cnt in zip(users, range(len(users))):
+        if user.id == user_id:
+            users.pop(cnt)
+            return user
+
+    raise HTTPException(status_code=404, detail='User was not found')
